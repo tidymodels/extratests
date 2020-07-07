6ba4b816102aa8c825b915accc2800bfac9e6744
Max Kuhn <mxkuhn@gmail.com>
2020-07-07 09:47:05 -0400

    fix spark test

--- tests/testthat/test-aaa-spark-install.R
+++ tests/testthat/test-aaa-spark-install.R
@@ -2,6 +2,6 @@ context("spark installation")
 
 sparklyr::spark_install()
 
-test_that('is spark installed', {
+test_that('is spark installed?', {
   expect_true(sparklyr::spark_install_find()$installed)
 })
