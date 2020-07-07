949d5121405e80c01523d6d241db43b85b40c43f
Max Kuhn <mxkuhn@gmail.com>
2020-07-07 16:12:11 -0400

    more spark install and more verbosity

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,18 @@
 library(testthat)
 library(extratests)
 
+## -----------------------------------------------------------------------------
+
+library(sparklyr)
+
+sparklyr::spark_install(verbose = TRUE)
+
+sc <- try(sparklyr::spark_connect(master = "local"), silent = TRUE)
+
+if(inherits(sc, "try-error")) {
+  print(sc)
+}
+
+## -----------------------------------------------------------------------------
+
 test_check("extratests", reporter = "summary")
--- tests/testthat/test-aaa-spark-install.R
+++ tests/testthat/test-aaa-spark-install.R
@@ -1,7 +1,5 @@
 context("spark installation")
 
-sparklyr::spark_install()
-
 test_that('is spark installed?', {
   expect_true(sparklyr::spark_install_find()$installed)
 })
