219666769dabc500f81d7741144b31e6439de22b
Max Kuhn <mxkuhn@gmail.com>
2020-07-07 19:58:28 -0400

    spark_install_winutils addition

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,9 +3,33 @@ library(extratests)
 
 ## -----------------------------------------------------------------------------
 
+spark_install_winutils <- function(version) {
+  hadoop_version <- if (version < "2.0.0") "2.6" else "2.7"
+  spark_dir <- paste("spark-", version, "-bin-hadoop", hadoop_version, sep = "")
+  winutils_dir <- file.path(Sys.getenv("LOCALAPPDATA"), "spark", spark_dir, "tmp", "hadoop", "bin", fsep = "\\")
+
+  if (!dir.exists(winutils_dir)) {
+    message("Installing winutils...")
+
+    dir.create(winutils_dir, recursive = TRUE)
+    winutils_path <- file.path(winutils_dir, "winutils.exe", fsep = "\\")
+
+    download.file(
+      "https://github.com/steveloughran/winutils/raw/master/hadoop-2.6.0/bin/winutils.exe",
+      winutils_path,
+      mode = "wb"
+    )
+
+    message("Installed winutils in ", winutils_path)
+  }
+}
+
+## -----------------------------------------------------------------------------
+
 library(sparklyr)
 
 if (.Platform$OS.type == "windows") {
+  spark_install_winutils("2.4")
   sparklyr::spark_install(verbose = TRUE, version = "2.4", hadoop_version = "2.7")
 } else {
   sparklyr::spark_install(verbose = TRUE, version = "2.4")
