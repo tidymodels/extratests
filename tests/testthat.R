06dcab4ea4aa26fe713dad2def21f1a6287b373e
Max Kuhn <mxkuhn@gmail.com>
2020-07-07 16:43:19 -0400

    hopefully better version numbers

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,9 +6,9 @@ library(extratests)
 library(sparklyr)
 
 if (.Platform$OS.type == "windows") {
-  sparklyr::spark_install(verbose = TRUE, hadoop_version = "2.6.0")
+  sparklyr::spark_install(verbose = TRUE, version = "2.4", hadoop_version = "2.7")
 } else {
-  sparklyr::spark_install(verbose = TRUE)
+  sparklyr::spark_install(verbose = TRUE, version = "2.4")
 }
 
 sc <- try(sparklyr::spark_connect(master = "local"), silent = TRUE)
