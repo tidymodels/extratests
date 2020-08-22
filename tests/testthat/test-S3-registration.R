2649032eae5315ae50266359d6b16228bc93bf64
Max Kuhn <mxkuhn@gmail.com>
2020-08-22 14:34:26 -0400

    test registration when generic is in different package

---
+++ tests/testthat/test-S3-registration.R
@@ -0,0 +1,28 @@
+context("S3 registration")
+
+# ------------------------------------------------------------------------------
+
+library(testthat)
+library(tune)
+library(recipes)
+
+test_recipe <-
+  recipe(mpg ~ ., data = mtcars) %>%
+  step_pls(all_predictors(), outcome = "mpg")
+
+
+# ------------------------------------------------------------------------------
+
+test_that('recipe tunable methods', {
+  pls_info <- tunable(test_recipe)
+  expect_true(tibble::is_tibble(pls_info))
+  expect_true(nrow(pls_info) > 0)
+})
+
+test_that('recipe required_pkgs methods', {
+  skip_if(utils::packageVersion("tune") <= "tune_0.1.1.9000")
+  skip_if(utils::packageVersion("recipe") <= "0.1.13.9000")
+  pls_pkgs <- required_pkgs(test_recipe, FALSE)
+
+  expect_equal(pls_pkgs, "mixOmics")
+})
