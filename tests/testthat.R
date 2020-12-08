068671455b4dc7430c23e06bcbae0f7d75c610a3
Julia Silge <julia.silge@gmail.com>
2020-12-08 16:56:51 -0700

    Update helper functions

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,20 +1,4 @@
 library(testthat)
 library(extratests)
 
-## -----------------------------------------------------------------------------
-
-spark_not_installed <- function() {
-
-  need_install <- try(sparklyr::spark_install_find(), silent = TRUE)
-
-  if(inherits(need_install, "try-error")) {
-    need_install <- TRUE
-  } else {
-    need_install <- !isTRUE(need_install$installed)
-  }
-  need_install
-}
-
-## -----------------------------------------------------------------------------
-
 test_check("extratests", reporter = "summary")
