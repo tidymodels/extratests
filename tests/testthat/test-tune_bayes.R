616130c217463e96beb852a243d465ab89101ca8
Hannah Frick <hfrick@users.noreply.github.com>
2022-02-16 19:20:13 +0000

    move from tune due to dependency on randomForest (#42)
    * move from tune due to dependency on randomForest
    
    randomForest now requires R >= 4.1.0
    
    * add dials to Suggests
    
    Co-authored-by: Julia Silge <julia.silge@gmail.com>

---
+++ tests/testthat/test-tune_bayes.R
@@ -0,0 +1,48 @@
+library(tidymodels)
+
+test_that('tune recipe and model, which has_unknowns', {
+
+  # This test needs a tuning parameter object with default 'unknown' parameter
+  # values (e.g., mtry).
+
+  skip_if_not_installed("randomForest")
+
+  rec_tune_1 <-
+    recipe(mpg ~ ., data = mtcars) %>%
+    step_normalize(all_predictors()) %>%
+    step_pca(all_predictors(), num_comp = tune())
+  rf_mod <-
+    rand_forest(mode = "regression", mtry = tune()) %>%
+    set_engine("randomForest")
+  iter1 <- 2
+  iter2 <- 2
+  iterT <- iter1 + iter2
+
+  set.seed(4400)
+  wflow <- workflow() %>% add_recipe(rec_tune_1) %>% add_model(rf_mod)
+  pset <- dials::parameters(wflow) %>% update(num_comp = num_comp(c(3, 5)),
+                                              mtry = mtry(c(1, 3)))
+  expect_true(
+    any(
+      vapply(
+        dials::parameters(wflow)$object,
+        dials::has_unknowns,
+        FUN.VALUE = TRUE
+      )
+    )
+  )
+  folds <- vfold_cv(mtcars)
+  res <- tune_bayes(wflow, resamples = folds, param_info = pset,
+                    initial = iter1, iter = iter2)
+  expect_equal(unique(res$id), folds$id)
+  expect_equal(
+    colnames(res$.metrics[[1]]),
+    c("mtry", "num_comp", ".metric", ".estimator", ".estimate", ".config")
+  )
+  res_est <- collect_metrics(res)
+  expect_equal(nrow(res_est), iterT * 2)
+  expect_equal(sum(res_est$.metric == "rmse"), iterT)
+  expect_equal(sum(res_est$.metric == "rsq"), iterT)
+  expect_equal(dplyr::n_distinct(res_est$.config), iterT)
+  expect_equal(res_est$n, rep(10, iterT * 2))
+})
