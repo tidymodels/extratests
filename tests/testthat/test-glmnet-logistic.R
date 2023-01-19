8fed3e281e9e3243feadb66599287e65484ef648
Hannah Frick <hannah@rstudio.com>
2023-01-19 15:45:23 +0000

    slim down test on class prediction

--- tests/testthat/test-glmnet-logistic.R
+++ tests/testthat/test-glmnet-logistic.R
@@ -31,61 +31,23 @@ test_that("glmnet execution and model object", {
   expect_equal(f_fit$fit[-12], exp_fit[-12])
 })
 
-test_that('glmnet prediction, one lambda', {
-
+test_that("glmnet prediction: column order of `new_data` irrelevant", {
   skip_if_not_installed("glmnet")
 
   data(lending_club, package = "modeldata", envir = rlang::current_env())
   lending_club <- head(lending_club, 200)
   num_pred <- c("funded_amnt", "annual_inc", "num_il_tl")
-  ctrl <- control_parsnip(verbosity = 1, catch = FALSE)
 
   xy_fit <- fit_xy(
     logistic_reg(penalty = 0.1) %>% set_engine("glmnet"),
-    control = ctrl,
     x = lending_club[, num_pred],
     y = lending_club$Class
   )
 
-  uni_pred <-
-    predict(xy_fit$fit,
-            newx = as.matrix(lending_club[1:7, num_pred]),
-            s = 0.1, type = "response")[,1]
-  uni_pred <- ifelse(uni_pred >= 0.5, "good", "bad")
-  uni_pred <- factor(uni_pred, levels = levels(lending_club$Class))
-  uni_pred <- unname(uni_pred)
-
-  expect_equal(uni_pred, predict(xy_fit, lending_club[1:7, num_pred])$.pred_class)
-  expect_equal(uni_pred[2], predict(xy_fit, lending_club[2, num_pred])$.pred_class)
   expect_equal(
-    predict(xy_fit, lending_club[1:7, num_pred]),
-    predict(xy_fit, lending_club[1:7, sample(num_pred)])
+    predict(xy_fit, lending_club[1:7, sample(num_pred)]),
+    predict(xy_fit, lending_club[1:7, num_pred])
   )
-
-
-  res_form <- fit(
-    logistic_reg(penalty = 0.1) %>% set_engine("glmnet"),
-    Class ~ log(funded_amnt) + int_rate,
-    data = lending_club,
-    control = ctrl
-  )
-
-  form_mat <- model.matrix(Class ~ log(funded_amnt) + int_rate, data = lending_club)
-  form_mat <- form_mat[1:7, -1]
-
-  form_pred <-
-    predict(res_form$fit,
-            newx = form_mat,
-            s = 0.1, type = "response")[,1]
-  form_pred <- ifelse(form_pred >= 0.5, "good", "bad")
-  form_pred <- factor(form_pred, levels = levels(lending_club$Class))
-  form_pred <- unname(form_pred)
-
-  expect_equal(
-    form_pred,
-    predict(res_form, lending_club[1:7, c("funded_amnt", "int_rate")], type = "class")$.pred_class
-  )
-
 })
 
 test_that('glmnet prediction, mulitiple lambda', {
