54e9e693d9daa715f38b1769727c98646d6b2d40
Max Kuhn <mxkuhn@gmail.com>
2023-01-19 20:43:31 -0500

    prediction tests

--- tests/testthat/test-censor-probs.R
+++ tests/testthat/test-censor-probs.R
@@ -7,6 +7,7 @@ test_that("reverse Kaplan-Meier curves", {
 
   library(survival)
   library(censored)
+  library(prodlim)
 
   lung <- lung[complete.cases(lung), ]
 
@@ -44,4 +45,21 @@ test_that("reverse Kaplan-Meier curves", {
   expect_equal(mod_fit$censor_probs$label, "reverse_km")
   expect_equal(mod_fit$censor_probs$required_pkgs, "prodlim")
   expect_snapshot(print(mod_fit$censor_probs))
+
+  # ----------------------------------------------------------------------------
+  # prediction
+
+  pred_times <- (7:10) * 100
+  parsnip_df_pred <- predict(mod_fit$censor_probs, time = pred_times)
+  parsnip_vec_pred <-
+    predict(mod_fit$censor_probs, time = pred_times, as_vector = TRUE)
+  pl_pred <-
+    predict(mod_fit$censor_probs$fit, times = pred_times, type = "surv")
+
+  expect_true(inherits(parsnip_df_pred, "tbl_df"))
+  expect_equal(names(parsnip_df_pred), ".prob_censored")
+  expect_equal(nrow(parsnip_df_pred), length(pred_times))
+  expect_equal(parsnip_df_pred[[1]], pl_pred)
+  expect_equal(parsnip_vec_pred, pl_pred)
+  expect_true(inherits(parsnip_vec_pred, "numeric"))
 })
