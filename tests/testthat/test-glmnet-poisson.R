e1301ef5e9d67a2187e43e84063fe18c2e208483
Hannah Frick <hannah@rstudio.com>
2023-01-21 18:46:35 +0000

    add tests on predict for types numeric and raw

--- tests/testthat/test-glmnet-poisson.R
+++ tests/testthat/test-glmnet-poisson.R
@@ -58,6 +58,100 @@ test_that('glmnet prediction, single lambda', {
   expect_equal(uni_pred, predict(res_form, seniors[1:5, 1:3])$.pred, tolerance = 0.0001)
 })
 
+test_that("glmnet prediction: type numeric", {
+  skip_if_not_installed("glmnet")
+
+  data(seniors, package = "poissonreg", envir = rlang::current_env())
+  seniors_x <- model.matrix(~ ., data = seniors[, -4])[, -1]
+  seniors_y <- seniors$count
+
+  exp_fit <- glmnet::glmnet(x = seniors_x, y = seniors_y, family = "poisson",
+                            alpha = 0.3, nlambda = 15)
+  exp_pred <- predict(exp_fit, seniors_x, s = 0.1, type = "response")
+
+  spec <- poisson_reg(penalty = 0.1, mixture = 0.3) %>%
+    set_engine("glmnet", nlambda = 15)
+  f_fit <- fit(spec, count ~ ., data = seniors)
+  xy_fit <- fit_xy(spec, x = seniors_x, y = seniors_y)
+
+  f_pred <- predict(f_fit, seniors)
+  xy_pred <- predict(xy_fit, seniors_x)
+  expect_equal(f_pred, xy_pred)
+  expect_equal(f_pred$.pred, as.vector(exp_pred))
+
+  # check format
+  expect_s3_class(f_pred, "tbl_df")
+  expect_equal(names(f_pred), ".pred")
+  expect_equal(nrow(f_pred), nrow(seniors))
+
+  # single prediction
+  f_pred_1 <- predict(f_fit, seniors[1, ])
+  expect_equal(nrow(f_pred_1), 1)
+  xy_pred_1 <- predict(xy_fit, seniors_x[1, , drop = FALSE])
+  expect_equal(nrow(xy_pred_1), 1)
+})
+
+test_that('glmnet prediction: column order of `new_data` irrelevant', {
+  skip_if_not_installed("glmnet")
+
+  data(seniors, package = "poissonreg", envir = rlang::current_env())
+  seniors_x <- model.matrix(~ ., data = seniors[, -4])[, -1]
+  seniors_y <- seniors$count
+
+  spec <- poisson_reg(penalty = 0.1, mixture = 0.3) %>%
+    set_engine("glmnet", nlambda = 15)
+  xy_fit <- fit_xy(spec, x = seniors_x, y = seniors_y)
+
+  expect_equal(
+    predict(xy_fit, seniors_x[, 3:1]),
+    predict(xy_fit, seniors_x)
+  )
+})
+
+test_that("glmnet prediction: type raw", {
+  skip_if_not_installed("glmnet")
+
+  data(seniors, package = "poissonreg", envir = rlang::current_env())
+  seniors_x <- model.matrix(~ ., data = seniors[, -4])[, -1]
+  seniors_y <- seniors$count
+
+  exp_fit <- glmnet::glmnet(x = seniors_x, y = seniors_y, family = "poisson",
+                            alpha = 0.3, nlambda = 15)
+  exp_pred <- predict(exp_fit, seniors_x, s = 0.1)
+
+  spec <- poisson_reg(penalty = 0.1, mixture = 0.3) %>%
+    set_engine("glmnet", nlambda = 15)
+  f_fit <- fit(spec, count ~ ., data = seniors)
+  xy_fit <- fit_xy(spec, x = seniors_x, y = seniors_y)
+
+  f_pred <- predict(f_fit, seniors, type = "raw")
+  xy_pred <- predict(xy_fit, seniors_x, type = "raw")
+  expect_equal(f_pred, xy_pred)
+  expect_equal(f_pred, exp_pred)
+
+  # single prediction
+  f_pred_1 <- predict(f_fit, seniors[1, ], type = "raw")
+  expect_equal(nrow(f_pred_1), 1)
+  xy_pred_1 <- predict(xy_fit, seniors_x[1, , drop = FALSE], type = "raw")
+  expect_equal(nrow(xy_pred_1), 1)
+})
+
+test_that("formula interface can deal with missing values", {
+  skip_if_not_installed("glmnet")
+
+  data(seniors, package = "poissonreg", envir = rlang::current_env())
+
+  seniors$alcohol[1] <- NA
+
+  spec <- poisson_reg(penalty = 0.1, mixture = 0.3) %>%
+    set_engine("glmnet", nlambda = 15)
+  f_fit <- fit(spec, count ~ ., data = seniors)
+
+  f_pred <- predict(f_fit, seniors)
+  expect_equal(nrow(f_pred), nrow(seniors))
+  expect_true(is.na(f_pred$.pred[1]))
+})
+
 test_that('error traps', {
   skip_on_cran()
   skip_if_not_installed("glmnet")
