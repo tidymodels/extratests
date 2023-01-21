6ac8b796008c04ebdcf88911dffcf23ffa43f935
Hannah Frick <hannah@rstudio.com>
2023-01-21 18:47:11 +0000

    remove previous test on predict()

--- tests/testthat/test-glmnet-poisson.R
+++ tests/testthat/test-glmnet-poisson.R
@@ -25,39 +25,6 @@ test_that("glmnet model object", {
   expect_equal(f_fit$fit[-11], exp_fit[-11])
 })
 
-test_that('glmnet prediction, single lambda', {
-  skip_on_cran()
-  skip_if_not_installed("glmnet")
-
-  data(seniors, package = "poissonreg", envir = rlang::current_env())
-  senior_ind <- model.matrix(~ ., data = seniors)[, -1]
-  senior_ind <- tibble::as_tibble(senior_ind)
-
-  glmn_spec <- poisson_reg(penalty = .01, mixture = .3) %>% set_engine("glmnet")
-  ctrl <- control_parsnip(verbosity = 1, catch = FALSE)
-
-  res_xy <- fit_xy(
-    glmn_spec,
-    control = ctrl,
-    x = senior_ind[, 1:3],
-    y = senior_ind$count
-  )
-
-  uni_pred <- c(538.72595867201, 735.342850038907, 283.116497320853, 386.444515401008,
-                92.1247665366651)
-
-  expect_equal(uni_pred, predict(res_xy, senior_ind[1:5, 1:3])$.pred, tolerance = 0.0001)
-
-  res_form <- fit(
-    glmn_spec,
-    count ~ .,
-    data = seniors,
-    control = ctrl
-  )
-
-  expect_equal(uni_pred, predict(res_form, seniors[1:5, 1:3])$.pred, tolerance = 0.0001)
-})
-
 test_that("glmnet prediction: type numeric", {
   skip_if_not_installed("glmnet")
 
