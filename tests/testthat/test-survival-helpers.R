4c3798f5e264b2a51ab7ab516b5f64c691e2644f
Max Kuhn <mxkuhn@gmail.com>
2023-02-28 20:32:57 -0500

    namespaces since functions are now not exported

--- tests/testthat/test-survival-helpers.R
+++ tests/testthat/test-survival-helpers.R
@@ -16,40 +16,40 @@ test_that('survival helpers', {
   expect_false(parsnip:::.is_surv(1, fail = FALSE))
   expect_snapshot_error(parsnip:::.is_surv(1))
 
-  expect_true(.is_censored_right(right_c))
-  expect_false(.is_censored_right(left_c))
+  expect_true(parsnip:::.is_censored_right(right_c))
+  expect_false(parsnip:::.is_censored_right(left_c))
 
   expect_equal(
-    .extract_surv_time(right_c),
+    parsnip:::.extract_surv_time(right_c),
     times
   )
   expect_equal(
-    .extract_surv_time(left_c),
+    parsnip:::.extract_surv_time(left_c),
     times
   )
   expect_equal(
-    .extract_surv_time(intv_c),
+    parsnip:::.extract_surv_time(intv_c),
     tibble::tibble(time1 = times, time2 = rep(1.0, 5))
   )
   expect_equal(
-    .extract_surv_time(count_c),
+    parsnip:::.extract_surv_time(count_c),
     tibble::tibble(start = times, stop = times2)
   )
 
   expect_equal(
-    .extract_surv_status(right_c),
+    parsnip:::.extract_surv_status(right_c),
     events
   )
   expect_equal(
-    .extract_surv_status(left_c),
+    parsnip:::.extract_surv_status(left_c),
     events
   )
   expect_equal(
-    .extract_surv_status(intv_c),
+    parsnip:::.extract_surv_status(intv_c),
     events
   )
   expect_equal(
-    .extract_surv_status(count_c),
+    parsnip:::.extract_surv_status(count_c),
     events
   )
 
