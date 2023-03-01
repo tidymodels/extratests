f330907fc339ef204ac763d453ae0b0e6ae9731d
topepo <mxkuhn@gmail.com>
2023-03-01 14:11:07 -0500

    updates from reviewer feedback

--- tests/testthat/_snaps/survival-helpers.md
+++ tests/testthat/_snaps/survival-helpers.md
@@ -2,3 +2,17 @@
 
     The object does not have class `Surv`.
 
+---
+
+    Code
+      parsnip:::.check_cens_type(left_c, fail = TRUE)
+    Error <rlang_error>
+      For this usage, the allowed censoring type is: 'right'
+
+---
+
+    Code
+      parsnip:::.check_cens_type(left_c, type = c("right", "interval"), fail = TRUE)
+    Error <rlang_error>
+      For this usage, the allowed censoring types are: 'right' and 'interval'
+
--- tests/testthat/test-survival-helpers.R
+++ tests/testthat/test-survival-helpers.R
@@ -1,7 +1,6 @@
 test_that('survival helpers', {
   skip_if_not_installed("parsnip", minimum_version = "1.0.4.9001")
   library(survival)
-  library(parsnip)
 
   times <- seq(1, 100, length.out = 5)
   times2 <- seq(100, 200, length.out = 5)
@@ -15,6 +14,15 @@ test_that('survival helpers', {
   expect_true(parsnip:::.is_surv(right_c))
   expect_false(parsnip:::.is_surv(1, fail = FALSE))
   expect_snapshot_error(parsnip:::.is_surv(1))
+
+  expect_snapshot(
+    error = TRUE,
+    parsnip:::.check_cens_type(left_c, fail = TRUE)
+  )
+  expect_snapshot(
+    error = TRUE,
+    parsnip:::.check_cens_type(left_c, type = c("right", "interval"), fail = TRUE)
+  )
 
   expect_true(parsnip:::.is_censored_right(right_c))
   expect_false(parsnip:::.is_censored_right(left_c))
@@ -51,6 +59,28 @@ test_that('survival helpers', {
   expect_equal(
     parsnip:::.extract_surv_status(count_c),
     events
+  )
+
+  # ------------------------------------------------------------------------------
+
+  events_interval_full <- c(1, 0:3)
+  events_interval_12 <- c(1, 2, 1, 2, 1)
+
+  intv_c <- Surv(times, times2, events_interval_full, type = "interval")
+  intv_c_12 <- Surv(times, times2, events_interval_12, type = "interval")
+
+  expect_equal(
+    parsnip:::.extract_surv_time(intv_c),
+    tibble::tibble(time1 = times, time2 = c(rep(1.0, 4), 200))
+  )
+
+  expect_equal(
+    parsnip:::.extract_surv_status(intv_c),
+    events_interval_full
+  )
+  expect_equal(
+    parsnip:::.extract_surv_status(intv_c_12),
+    events_interval_12
   )
 
 })
