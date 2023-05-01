ef44c62f184282b8fc8d881b431824ee83bc7fd7
simonpcouch <simonpatrickcouch@gmail.com>
2023-05-01 13:01:45 -0400

    test yardstick known selection caching

---
+++ tests/testthat/test-yardstick-known-selections.R
@@ -0,0 +1,54 @@
+library(tune)
+library(rsample)
+library(parsnip)
+library(yardstick)
+library(modeldata)
+
+skip_if_not_installed("yardstick", minimum_version = "1.2.0.9001")
+
+test_that("known selections don't kick in outside of catalog env", {
+  tune_env <- rlang::ns_env("tune")$tune_env
+  expect_null(tune_env$known_selections)
+
+  test_res <- rmse(two_class_dat, A, B)
+
+  expect_null(tune_env$known_selections)
+})
+
+test_that("known selections are cleared after tune runs", {
+  tune_env <- rlang::ns_env("tune")$tune_env
+  expect_null(tune_env$known_selections)
+
+  fit_resamples(
+    logistic_reg(),
+    Class ~ .,
+    bootstraps(two_class_dat, 5)
+  )
+
+  expect_null(tune_env$known_selections)
+})
+
+test_that("known selections are stored", {
+  extract_known_selections <- function(x) {
+    rlang::ns_env("tune")$tune_env$known_selections
+  }
+
+  res <-
+    fit_resamples(
+      logistic_reg(),
+      Class ~ .,
+      bootstraps(two_class_dat, 5),
+      control = control_resamples(extract = extract_known_selections)
+    )
+
+  known_selections <- collect_extracts(res)[[5, ".extracts"]]
+
+  expect_equal(
+    known_selections,
+    list(list(
+      Class = "Class",
+      .pred_class = ".pred_class",
+      `~".pred_Class1"` = ".pred_Class1"
+    ))
+  )
+})
