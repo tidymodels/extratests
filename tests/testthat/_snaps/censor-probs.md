23811ff302998859c0f3888589d50c0cfcec85d1
Max Kuhn <mxkuhn@gmail.com>
2023-05-02 16:15:07 -0400

    unit tests for censoring probabilities (#67)
    * unit tests for tidymodels/parsnip#855
    
    * prediction tests
    
    * Apply suggestions from code review
    
    Co-authored-by: Hannah Frick <hfrick@users.noreply.github.com>
    
    * updated based on parsnip changes
    
    * bumped version
    
    * censored attaches survival
    
    ---------
    
    Co-authored-by: Hannah Frick <hfrick@users.noreply.github.com>
    Co-authored-by: Hannah Frick <hannah@rstudio.com>

---
+++ tests/testthat/_snaps/censor-probs.md
@@ -0,0 +1,44 @@
+# reverse Kaplan-Meier curves
+
+    Code
+      print(mod_fit$censor_probs)
+    Output
+      $formula
+      Surv(time, status) ~ age + sex
+      <environment: base>
+      
+      $fit
+      
+      Call: prodlim::prodlim(formula = Surv(time, status) ~ 1, data = eval_env$data, 
+          reverse = TRUE, x = FALSE, type = "surv")
+      
+    Message <simpleMessage>
+      Kaplan-Meier estimator for the censoring time survival function
+    Output
+      
+    Message <simpleMessage>
+      No covariates
+    Output
+      
+      Right-censored response of a survival model
+      
+      No.Observations: 167 
+      
+      Pattern:
+                      Freq
+       event          120 
+       right.censored 47  
+      
+      $label
+      [1] "reverse_km"
+      
+      $required_pkgs
+      [1] "prodlim"
+      
+      attr(,"class")
+      [1] "censoring_model_reverse_km" "censoring_model"           
+
+---
+
+    no applicable method for 'predict' applied to an object of class "censoring_model"
+
---
+++ tests/testthat/test-censor-probs.R
@@ -0,0 +1,108 @@
+
+test_that("reverse Kaplan-Meier curves", {
+  skip_if_not_installed("prodlim")
+  skip_if_not_installed("censored")
+  skip_if_not_installed("parsnip", minimum_version = "1.0.3.9003")
+
+  library(censored)
+  library(prodlim)
+
+  # ----------------------------------------------------------------------------
+
+  lung <- lung[complete.cases(lung), ]
+
+  mod_fit <-
+    survival_reg() %>%
+    fit(Surv(time, status) ~ age + sex, data = lung)
+
+  # For testing purposes
+  attr(mod_fit$censor_probs$formula, ".Environment") <- rlang::base_env()
+
+  psnip_df <- as_tibble(mod_fit$censor_probs$fit[1:6])
+
+  alt_obj <- mod_fit$censor_probs
+  class(alt_obj) <- "censoring_model"
+
+  prdlim <-
+    prodlim::prodlim(
+      Surv(time, status) ~ 1,
+      data = lung,
+      reverse = TRUE,
+      type = "surv"
+    )
+  prdlim_df <- as_tibble(prdlim[1:6])
+
+  # ----------------------------------------------------------------------------
+
+  expect_snapshot( print(mod_fit$censor_probs) )
+  expect_true(any(names(mod_fit) == "censor_probs"))
+  expect_true(
+    inherits(mod_fit$censor_probs,
+             c("censoring_model_reverse_km", "censoring_model")))
+  expect_equal(
+    names(mod_fit$censor_probs),
+    c("formula", "fit", "label", "required_pkgs")
+  )
+  expect_true(inherits(mod_fit$censor_probs$fit, "prodlim"))
+  expect_equal(
+    mod_fit$censor_probs$formula,
+    Surv(time, status) ~ age + sex,
+    ignore_formula_env = TRUE
+  )
+  expect_equal(
+    mod_fit$censor_probs$fit$formula,
+    Surv(time, status) ~ 1,
+    ignore_formula_env = TRUE
+  )
+
+  expect_equal(mod_fit$censor_probs$label, "reverse_km")
+  expect_equal(mod_fit$censor_probs$required_pkgs, "prodlim")
+  expect_equal(
+    prdlim_df,
+    psnip_df
+  )
+
+  # ----------------------------------------------------------------------------
+  # prediction
+
+  pred_times <- (7:10) * 100
+  parsnip_df_pred <- predict(mod_fit$censor_probs, time = pred_times)
+  parsnip_vec_pred <-
+    predict(mod_fit$censor_probs, time = pred_times, as_vector = TRUE)
+  pl_pred <-
+    predict(mod_fit$censor_probs$fit, times = pred_times, type = "surv")
+
+  expect_true(inherits(parsnip_df_pred, "tbl_df"))
+  expect_equal(names(parsnip_df_pred), ".prob_censored")
+  expect_equal(nrow(parsnip_df_pred), length(pred_times))
+  expect_equal(parsnip_df_pred[[1]], pl_pred)
+  expect_equal(parsnip_vec_pred, pl_pred)
+  expect_true(inherits(parsnip_vec_pred, "numeric"))
+
+  miss_pred <-
+    predict(mod_fit$censor_probs,
+            time = c(NA_real_, pred_times),
+            as_vector = TRUE)
+  expect_equal(
+    length(miss_pred),
+    length(pred_times) + 1
+  )
+  expect_equal(
+    sum(is.na(miss_pred)),
+    1L
+  )
+  expect_equal(
+    which(is.na(miss_pred)),
+    1
+  )
+  # ----------------------------------------------------------------------------
+  #
+
+  expect_snapshot_error( predict(alt_obj, time = test_times) )
+  expect_equal(
+    parsnip:::reverse_km(linear_reg(), NULL),
+    list()
+  )
+
+})
+
