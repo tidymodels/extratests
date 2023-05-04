2ec3bfa92ed90e01711c7f21c6b749ada9acf19c
topepo <mxkuhn@gmail.com>
2023-05-04 14:09:41 -0400

    nocov

---
+++ tests/testthat/_snaps/test-survival-augment.R
@@ -0,0 +1,59 @@
+library(testthat)
+library(tidymodels)
+library(prodlim)
+library(censored)
+
+
+# ------------------------------------------------------------------------------
+
+test_that('augmenting survival models ', {
+  skip_if_not_installed("parsnip", minimum_version = "1.1.0.9001")
+
+  set.seed(1)
+  sim_dat <- SimSurv(500) %>%
+    mutate(event_time = Surv(time, event)) %>%
+    select(event_time, X1, X2)
+
+  set.seed(2)
+  split <- initial_split(sim_dat)
+  sim_tr <- training(split)
+  sim_te <- testing(split)
+
+  time_points <- c(10, 1, 5, 15)
+
+  # ------------------------------------------------------------------------------
+
+  sr_fit <-
+    survival_reg() %>%
+    fit(event_time ~ ., data = sim_tr)
+
+  expect_snapshot_error(augment(sr_fit, new_data = sim_tr))
+
+  sr_aug <- augment(sr_fit, new_data = sim_tr, eval_time = time_points)
+  expect_equal(nrow(sim_tr), nrow(sr_aug))
+  expect_equal(names(sr_aug), c(".pred", ".pred_time", "event_time", "X1", "X2"))
+  expect_true(is.list(sr_aug$.pred))
+  expect_equal(
+    names(sr_aug$.pred[[1]]),
+    c(".eval_time", ".pred_survival", ".weight_time", ".pred_censored",
+      ".weight_censored")
+  )
+
+  # ------------------------------------------------------------------------------
+
+  glmn_fit <-
+    proportional_hazards(penalty = 0.1) %>%
+    set_engine("glmnet") %>%
+    fit(event_time ~ ., data = sim_tr)
+
+  glmn_aug <- augment(glmn_fit, new_data = sim_tr, eval_time = time_points)
+  expect_equal(nrow(sim_tr), nrow(glmn_aug))
+  expect_equal(names(glmn_aug), c(".pred", ".pred_time", "event_time", "X1", "X2"))
+  expect_true(is.list(glmn_aug$.pred))
+  expect_equal(
+    names(glmn_aug$.pred[[1]]),
+    c(".eval_time", ".pred_survival", ".weight_time", ".pred_censored",
+      ".weight_censored")
+  )
+
+})
