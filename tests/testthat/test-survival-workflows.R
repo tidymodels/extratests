943a61f9c09a7225b1c11903604ecdc6e6d93b42
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-07-21 15:00:45 -0700

    add add_variables() tests

--- tests/testthat/test-survival-workflows.R
+++ tests/testthat/test-survival-workflows.R
@@ -26,6 +26,23 @@ test_that("can `fit()` a censored workflow with a formula", {
   )
 })
 
+test_that("can `fit()` a censored workflow with variables", {
+  mod <- proportional_hazards(engine = "glmnet", penalty = 0.1)
+
+  workflow <- workflow()
+  workflow <- add_variables(workflow, outcomes = surv, predictors = everything())
+  workflow <- add_model(workflow, mod)
+
+  wf_fit <- fit(workflow, lung)
+
+  expect_s3_class(wf_fit$fit$fit, "model_fit")
+
+  expect_equal(
+    wf_fit$fit$fit$fit$fit$beta,
+    censored::coxnet_train(surv ~ ., data = lung)$fit$beta
+  )
+})
+
 test_that("can `fit()` a censored workflow with a recipe", {
   rec <- recipes::recipe(surv ~ ., lung)
 
@@ -67,6 +84,36 @@ test_that("can `predict()` a censored workflow with a formula", {
     all(purrr::map_lgl(
       preds$.pred,
        ~ identical(names(.x), c(".eval_time", ".pred_survival"))
+    ))
+  )
+
+  expect_error(
+    predict(wf_fit, new_data = lung, type = "numeric")
+  )
+})
+
+test_that("can `predict()` a censored workflow with a recipe", {
+  mod <- proportional_hazards(engine = "glmnet", penalty = 0.1)
+
+  workflow <- workflow()
+  workflow <- add_variables(workflow, outcomes = surv, predictors = everything())
+  workflow <- add_model(workflow, mod)
+
+  wf_fit <- fit(workflow, lung)
+
+  preds <- predict(wf_fit, new_data = lung)
+
+  expect_identical(names(preds), ".pred_time")
+  expect_type(preds$.pred_time, "double")
+
+  preds <- predict(wf_fit, new_data = lung, type = "survival", eval_time = c(100, 200))
+
+  expect_identical(names(preds), ".pred")
+  expect_type(preds$.pred, "list")
+  expect_true(
+    all(purrr::map_lgl(
+      preds$.pred,
+      ~ identical(names(.x), c(".eval_time", ".pred_survival"))
     ))
   )
 
