4326509c645d269017b2e71a248c3247d3e40860
simonpcouch <simonpatrickcouch@gmail.com>
2023-11-06 09:11:48 -0600

    test parsnip messaging re: model formulas

---
+++ tests/testthat/test-parsnip-model-formula.R
@@ -0,0 +1,50 @@
+test_that('error with model formula (workflow, no tune)', {
+  skip_if(utils::packageVersion("parsnip") < "1.1.1.9001")
+
+  library(parsnip)
+  library(workflows)
+
+  gam_wflow <- workflow() %>%
+    add_formula(mpg ~ .) %>%
+    add_model(gen_additive_mod("regression"))
+
+  expect_snapshot(
+    error = TRUE,
+    gam_fit <- gam_wflow %>% fit(mtcars)
+  )
+})
+
+test_that('error with model formula (workflow, with tune)', {
+  skip_if(utils::packageVersion("parsnip") < "1.1.1.9001")
+
+  library(parsnip)
+  library(workflows)
+  library(tune)
+  library(rsample)
+
+  gam_wflow <- workflow() %>%
+    add_formula(mpg ~ .) %>%
+    add_model(gen_additive_mod("regression"))
+
+  expect_warning(
+    gam_res <- gam_wflow %>% fit_resamples(bootstraps(mtcars))
+  )
+
+  expect_snapshot(show_notes(gam_res))
+})
+
+test_that('error with model formula (no workflow, with tune)', {
+  skip_if(utils::packageVersion("parsnip") < "1.1.1.9001")
+
+  library(parsnip)
+  library(tune)
+  library(rsample)
+
+  gam_spec <- gen_additive_mod("regression")
+
+  expect_warning(
+    gam_res <- gam_spec %>% fit_resamples(mpg ~ ., bootstraps(mtcars))
+  )
+
+  expect_snapshot(show_notes(gam_res))
+})
