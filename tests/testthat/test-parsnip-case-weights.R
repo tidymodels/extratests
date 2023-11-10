353f2d6aace892aeb07539c474ed4da1e9bdebb0
Hannah Frick <hannah@posit.co>
2023-11-10 18:53:29 +0000

    split out tests for discrim

--- tests/testthat/test-parsnip-case-weights.R
+++ tests/testthat/test-parsnip-case-weights.R
@@ -10,7 +10,6 @@ library(parsnip)
 library(rules)
 library(poissonreg)
 library(multilevelmod)
-library(discrim)
 library(sparklyr)
 
 
@@ -67,7 +66,6 @@ test_that('boost_tree - C50 case weights', {
   expect_unequal(unwt_fit$fit$tree, wt_fit$fit$tree)
 
 })
-
 
 
 # C5_rules ----------------------------------------------------------------
@@ -167,60 +165,6 @@ test_that('decision_tree - C50 case weights', {
 
   expect_true(wt_fit$fit$caseWeights)
   expect_unequal(unwt_fit$fit$tree, wt_fit$fit$tree)
-})
-
-
-# discrim_flexible --------------------------------------------------------
-
-test_that('discrim_flexible - earth case weights', {
-  skip_if_not_installed("discrim", "1.0.0")
-
-  dat <- make_two_class_wts()
-
-  expect_error({
-    wt_fit <-
-      discrim_flexible(prune_method = "none") %>%
-      set_engine("earth") %>%
-      fit(Class ~ ., data = two_class_dat, case_weights = dat$wts)
-  },
-  regexp = NA)
-
-  unwt_fit <-
-    discrim_flexible(prune_method = "none") %>%
-    set_engine("earth") %>%
-    fit(Class ~ ., data = two_class_dat)
-
-  expect_snapshot(print(wt_fit$fit$call))
-  expect_unequal(unwt_fit$fit$fit$coefficients, wt_fit$fit$fit$coefficients)
-})
-
-
-# discrim_linear ----------------------------------------------------------
-
-test_that('LDA - sda case weights', {
-  skip_if_not_installed("discrim", "1.0.0")
-
-  dat <- make_two_class_wts()
-
-  data("two_class_dat", package = "modeldata")
-  wts <- order(-two_class_dat$B)
-  wts <- importance_weights(wts)
-
-  expect_error({
-    wt_fit <-
-      discrim_linear(penalty = 0.0001) %>%
-      set_engine("mda") %>%
-      fit(Class ~ ., data = two_class_dat, case_weights = dat$wts)
-  },
-  regexp = NA)
-
-  unwt_fit <-
-    discrim_linear(penalty = 0.0001) %>%
-    set_engine("mda") %>%
-    fit(Class ~ ., data = two_class_dat)
-
-  expect_snapshot(wt_fit$fit$call)
-  expect_unequal(unwt_fit$fit$fit, wt_fit$fit$fit)
 })
 
 
