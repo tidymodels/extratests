15ecc3abeb4d2939e51f57475678a0eb47dc297e
Hannah Frick <hannah@posit.co>
2023-11-10 18:57:34 +0000

    split out tests for rules

--- tests/testthat/test-parsnip-case-weights.R
+++ tests/testthat/test-parsnip-case-weights.R
@@ -7,7 +7,6 @@ skip_if_not_installed("recipes",   "1.0.0")
 
 # load all extension packages to register the engines
 library(parsnip)
-library(rules)
 library(poissonreg)
 library(multilevelmod)
 library(sparklyr)
@@ -65,58 +64,6 @@ test_that('boost_tree - C50 case weights', {
   expect_true(wt_fit$fit$caseWeights)
   expect_unequal(unwt_fit$fit$tree, wt_fit$fit$tree)
 
-})
-
-
-# C5_rules ----------------------------------------------------------------
-
-test_that('C5_rules - C50 case weights', {
-  skip_if_not_installed("rules", "1.0.0")
-
-  data("two_class_dat", package = "modeldata")
-  wts <- order(-two_class_dat$B)
-  wts <- importance_weights(wts)
-
-  expect_error({
-    wt_fit <-
-      C5_rules(trees = 5) %>%
-      set_engine("C5.0") %>%
-      set_mode("classification") %>%
-      fit(Class ~ ., data = two_class_dat, case_weights = wts)
-  },
-  regexp = NA)
-
-  unwt_fit <-
-    C5_rules(trees = 5) %>%
-    set_engine("C5.0") %>%
-    set_mode("classification") %>%
-    fit(Class ~ ., data = two_class_dat)
-
-  expect_true(wt_fit$fit$caseWeights)
-  expect_unequal(unwt_fit$fit$rules, wt_fit$fit$rules)
-
-})
-
-
-# cubist_rules ------------------------------------------------------------
-
-test_that('cubist case weights', {
-  skip_if_not_installed("rules", "1.0.0")
-
-  dat <- make_ames_wts()
-
-  expect_error(
-    wt_fit <-
-      cubist_rules() %>%
-      fit(Sale_Price ~ Longitude + Latitude, data = dat$full, case_weights = dat$wts),
-    regexp = NA)
-
-  unwt_fit <-
-    cubist_rules() %>%
-    fit(Sale_Price ~ Longitude + Latitude, data = dat$full)
-
-  expect_unequal(wt_fit$fit$model, unwt_fit$fit$model)
-  expect_true(wt_fit$fit$caseWeights)
 })
 
 
