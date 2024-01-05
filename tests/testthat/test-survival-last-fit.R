d92b36156d3d669330a136f90109b144cb179eec
‘topepo’ <mxkuhn@gmail.com>
2024-01-05 11:34:35 -0500

    update tune version

--- tests/testthat/test-survival-last-fit.R
+++ tests/testthat/test-survival-last-fit.R
@@ -3,7 +3,7 @@ suppressPackageStartupMessages(library(censored))
 
 skip_if_not_installed("parsnip", minimum_version = "1.1.0.9003")
 skip_if_not_installed("censored", minimum_version = "0.2.0.9000")
-skip_if_not_installed("tune", minimum_version = "1.1.1.9001")
+skip_if_not_installed("tune", minimum_version = "1.1.2.9009")
 skip_if_not_installed("yardstick", minimum_version = "1.2.0.9001")
 
 test_that("last fit for survival models with static metric", {
