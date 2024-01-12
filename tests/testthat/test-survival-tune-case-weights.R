1968bd9ce4160a3b6234390480f503b35dbb9c8c
‘topepo’ <mxkuhn@gmail.com>
2024-01-12 12:51:10 -0500

    bump version and add freq weights

--- tests/testthat/test-survival-tune-case-weights.R
+++ tests/testthat/test-survival-tune-case-weights.R
@@ -4,7 +4,7 @@ suppressPackageStartupMessages(library(censored))
 skip_if_not_installed("parsnip", minimum_version = "1.1.0.9003")
 skip_if_not_installed("censored", minimum_version = "0.2.0.9000")
 skip_if_not_installed("tune", minimum_version = "1.1.2.9010")
-skip_if_not_installed("yardstick", minimum_version = "1.2.0.9001")
+skip_if_not_installed("yardstick", minimum_version = "1.2.0.9003")
 
 test_that("grid tuning survival models with case weights", {
   skip_if_not_installed("prodlim")
@@ -100,57 +100,50 @@ test_that("grid tuning survival models with case weights", {
 
   ## freq weighted -------------------------------------------------------------
 
-  # → A | error:   Failed to compute `brier_survival()`.
-  #                Caused by error:
-  #                ℹ In argument: `.estimate = brier_survival_impl(...)`.
-  #                ℹ In group 1: `.eval_time = 1`.
-  #                Caused by error in `vec_math()`:
-  #                ! `vec_math.hardhat_frequency_weights()` not implemented.
+  set.seed(3)
+  wts <- c(rep(0L, 50), rep(1L, nrow(sim_dat) - 50))
 
-  # set.seed(3)
-  # wts <- c(rep(0L, 50), rep(1L, nrow(sim_dat) - 50))
-  #
-  # frq_wts <- sim_dat %>%
-  #   mutate(frq_wts = frequency_weights(wts))
-  #
-  # set.seed(2)
-  # frq_split <- initial_split(frq_wts)
-  # frq_tr <- training(frq_split)
-  # frq_rs <- vfold_cv(frq_tr)
-  #
-  # frq_wflow <-
-  #   workflow() %>%
-  #   add_model(mod_spec) %>%
-  #   add_formula(event_time ~ X1 + X2) %>%
-  #   add_case_weights(frq_wts)
-  #
-  # set.seed(2193)
-  # weights_frq_tune_res <-
-  #   frq_wflow %>%
-  #   tune_grid(
-  #     resamples = frq_rs,
-  #     grid = grid,
-  #     metrics = mix_mtrc,
-  #     eval_time = time_points,
-  #     control = gctrl
-  #   )
-  #
-  # expect_false(
-  #   isTRUE(
-  #     all.equal(
-  #       weights_none_tune_res$.extracts[[1]]$.extracts[[1]],
-  #       weights_frq_tune_res$.extracts[[1]]$.extracts[[1]]
-  #     )
-  #   )
-  # )
-  #
-  # expect_false(
-  #   isTRUE(
-  #     all.equal(
-  #       collect_metrics(weights_none_tune_res),
-  #       collect_metrics(weights_frq_tune_res)
-  #     )
-  #   )
-  # )
+  frq_wts <- sim_dat %>%
+    mutate(frq_wts = frequency_weights(wts))
+
+  set.seed(2)
+  frq_split <- initial_split(frq_wts)
+  frq_tr <- training(frq_split)
+  frq_rs <- vfold_cv(frq_tr)
+
+  frq_wflow <-
+    workflow() %>%
+    add_model(mod_spec) %>%
+    add_formula(event_time ~ X1 + X2) %>%
+    add_case_weights(frq_wts)
+
+  set.seed(2193)
+  weights_frq_tune_res <-
+    frq_wflow %>%
+    tune_grid(
+      resamples = frq_rs,
+      grid = grid,
+      metrics = mix_mtrc,
+      eval_time = time_points,
+      control = gctrl
+    )
+
+  expect_false(
+    isTRUE(
+      all.equal(
+        weights_none_tune_res$.extracts[[1]]$.extracts[[1]],
+        weights_frq_tune_res$.extracts[[1]]$.extracts[[1]]
+      )
+    )
+  )
+
+  expect_false(
+    isTRUE(
+      all.equal(
+        collect_metrics(weights_none_tune_res),
+        collect_metrics(weights_frq_tune_res)
+      )
+    )
+  )
 
 })
