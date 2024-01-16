6eb7938fb1564d41fbc58a5990b1a7f9e51d743a
topepo <mxkuhn@gmail.com>
2024-01-16 15:36:27 -0500

    changes based on reviewer feedback

--- tests/testthat/test-survival-tune-case-weights.R
+++ tests/testthat/test-survival-tune-case-weights.R
@@ -6,7 +6,7 @@ skip_if_not_installed("censored", minimum_version = "0.2.0.9000")
 skip_if_not_installed("tune", minimum_version = "1.1.2.9010")
 skip_if_not_installed("yardstick", minimum_version = "1.2.0.9003")
 
-test_that("grid tuning survival models with case weights", {
+test_that("grid tuning survival models with importance case weights", {
   skip_if_not_installed("prodlim")
 
   ## ------------------------------------------------------------------------------
@@ -97,19 +97,58 @@ test_that("grid tuning survival models with case weights", {
     )
   )
 
+})
+
+
+
+test_that("grid tuning survival models with frequency case weights", {
+  skip_if_not_installed("prodlim")
+
+  ## ------------------------------------------------------------------------------
+
+  time_points <- c(10, 1, 5, 15)
+
+  grid <- tibble(penalty = 10^c(-4, -2, -1))
+
+  mod_spec <-
+    proportional_hazards(penalty = 0.01, mixture = 1) %>%
+    set_engine("glmnet", path_values = grid$penalty) %>%
+    set_mode("censored regression")
+
+  rctrl <- control_resamples(extract = function(x) extract_fit_engine(x))
+
+  mix_mtrc  <- metric_set(brier_survival, brier_survival_integrated, concordance_survival)
+
+  set.seed(1)
+  sim_dat <- prodlim::SimSurv(500) %>%
+    mutate(event_time = Surv(time, event)) %>%
+    select(event_time, X1, X2) %>%
+    as_tibble()
+
+  ## unweighted ----------------------------------------------------------------
+
+  sim_rs <- apparent(sim_dat %>% slice(-(1:50)))
+
+  set.seed(2193)
+  weights_none_tune_res <-
+    mod_spec %>%
+    fit_resamples(
+      event_time ~ X1 + X2,
+      resamples = sim_rs,
+      metrics = mix_mtrc,
+      eval_time = time_points,
+      control = rctrl
+    )
 
   ## freq weighted -------------------------------------------------------------
 
   set.seed(3)
-  wts <- c(rep(0L, 50), rep(1L, nrow(sim_dat) - 50))
+  wts <- c(rep(0, 50), rep(1L, nrow(sim_dat) - 50))
 
   frq_wts <- sim_dat %>%
     mutate(frq_wts = frequency_weights(wts))
 
-  set.seed(2)
-  frq_split <- initial_split(frq_wts)
-  frq_tr <- training(frq_split)
-  frq_rs <- vfold_cv(frq_tr)
+  frq_rs <- apparent(frq_wts)
 
   frq_wflow <-
     workflow() %>%
@@ -120,30 +159,17 @@ test_that("grid tuning survival models with case weights", {
   set.seed(2193)
   weights_frq_tune_res <-
     frq_wflow %>%
-    tune_grid(
+    fit_resamples(
       resamples = frq_rs,
-      grid = grid,
       metrics = mix_mtrc,
       eval_time = time_points,
-      control = gctrl
+      control = rctrl
     )
 
-  expect_false(
-    isTRUE(
-      all.equal(
-        weights_none_tune_res$.extracts[[1]]$.extracts[[1]],
-        weights_frq_tune_res$.extracts[[1]]$.extracts[[1]]
-      )
-    )
+  expect_equal(
+    coef(weights_none_tune_res$.extracts[[1]]$.extracts[[1]]),
+    coef(weights_frq_tune_res$.extracts[[1]]$.extracts[[1]])
   )
 
-  expect_false(
-    isTRUE(
-      all.equal(
-        collect_metrics(weights_none_tune_res),
-        collect_metrics(weights_frq_tune_res)
-      )
-    )
-  )
 
 })
