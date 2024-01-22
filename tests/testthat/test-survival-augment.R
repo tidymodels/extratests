ada1d0a4b8cc48962886b87c40e7401809868b73
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-01-22 15:51:38 -0800

    add tests for augment() last_fit()

--- tests/testthat/test-survival-augment.R
+++ tests/testthat/test-survival-augment.R
@@ -160,6 +160,49 @@ test_that("augment() works for resample_results", {
   )
 })
 
+test_that("augment() works for last fit", {
+  skip_if_not_installed("prodlim")
+
+  # standard setup start -------------------------------------------------------
+
+  set.seed(1)
+  sim_dat <- prodlim::SimSurv(500) %>%
+    mutate(event_time = Surv(time, event)) %>%
+    select(event_time, X1, X2)
+
+  set.seed(2)
+  split <- initial_split(sim_dat)
+  sim_tr <- training(split)
+  sim_te <- testing(split)
+  sim_rs <- vfold_cv(sim_tr)
+
+  time_points <- c(10, 1, 5, 15)
+
+  # last fit for models with a mixture of metrics ------------------------------
+
+  mix_mtrc  <- metric_set(brier_survival, brier_survival_integrated, concordance_survival)
+
+  set.seed(2193)
+  rs_mixed_res <-
+    survival_reg() %>%
+    last_fit(
+      event_time ~ X1 + X2,
+      split = split,
+      metrics = mix_mtrc,
+      eval_time = time_points
+    )
+
+  aug_res <- augment(rs_mixed_res)
+
+  expect_equal(nrow(aug_res), nrow(sim_te))
+  expect_equal(names(aug_res), c(".pred", ".pred_time", "event_time", "X1", "X2"))
+  expect_true(is.list(aug_res$.pred))
+  expect_equal(
+    names(aug_res$.pred[[1]]),
+    c(".eval_time", ".pred_survival", ".weight_censored")
+  )
+})
+
 test_that("augment() for survival models skips unavailble prediction type", {
   skip_if_not_installed("parsnip", minimum_version = "1.1.0.9001")
   skip_if_not_installed("prodlim")
