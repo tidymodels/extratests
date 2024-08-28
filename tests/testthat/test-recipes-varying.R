f13bff8be775442109dcd50083c6dc9623d8c7fc
topepo <mxkuhn@gmail.com>
2024-08-28 15:24:09 -0400

    update two other tests/snapshots

--- tests/testthat/test-recipes-varying.R
+++ tests/testthat/test-recipes-varying.R
@@ -65,9 +65,9 @@ test_that("recipe steps with non-varying args error if specified as varying()", 
   rec_bad_varying <- rec_1
   rec_bad_varying$steps[[1]]$skip <- varying()
 
-  expect_error(
+  expect_snapshot(
     varying_args(rec_bad_varying),
-    "The following argument for a recipe step of type 'step_center' is not allowed to vary: 'skip'."
+    error = TRUE
   )
 })
 
