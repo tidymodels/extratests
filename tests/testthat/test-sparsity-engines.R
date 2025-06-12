fde99707c7f93e743fcbfad461e7523b356aa2e3
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-06-12 15:37:00 -0700

    test lightgbm for sparsity

--- tests/testthat/test-sparsity-engines.R
+++ tests/testthat/test-sparsity-engines.R
@@ -46,6 +46,60 @@ test_that("boost_tree - xgboost - classification", {
   )
 })
 
+test_that("boost_tree - xgboost - regression", {
+  skip_if_not_installed("bonsai")
+  skip_if_not_installed("lightgbm")
+  skip_if_not_installed("modeldata")
+  skip_if_not_installed("hardhat", "1.4.1")
+  skip_if_not_installed("recipes", "1.1.1.9000")
+  skip_if_not_installed("parsnip", "1.3.0")
+  skip_if_not_installed("workflows", "1.2.0")
+
+  data("ames", package = "modeldata")
+
+  rec_spec <- recipe(Sale_Price ~ ., data = ames) %>%
+    step_dummy(all_nominal_predictors(), sparse = "yes")
+
+  library(bonsai)
+
+  mod_spec <- boost_tree("regression", "lightgbm")
+
+  wf_spec <- workflow(rec_spec, mod_spec)
+
+  withr::local_options("sparsevctrs.verbose_materialize" = 1)
+
+  expect_no_warning(
+    wf_fit <- fit(wf_spec, ames)
+  )
+})
+
+test_that("boost_tree - xgboost - classification", {
+  skip_if_not_installed("bonsai")
+  skip_if_not_installed("lightgbm")
+  skip_if_not_installed("modeldata")
+  skip_if_not_installed("hardhat", "1.4.1")
+  skip_if_not_installed("recipes", "1.1.1.9000")
+  skip_if_not_installed("parsnip", "1.3.0")
+  skip_if_not_installed("workflows", "1.2.0")
+
+  data("ames", package = "modeldata")
+
+  rec_spec <- recipe(Street ~ ., data = ames) %>%
+    step_dummy(all_nominal_predictors(), sparse = "yes")
+
+  library(bonsai)
+
+  mod_spec <- boost_tree("classification", "lightgbm")
+
+  wf_spec <- workflow(rec_spec, mod_spec)
+
+  withr::local_options("sparsevctrs.verbose_materialize" = 2)
+
+  expect_no_warning(
+    wf_fit <- fit(wf_spec, ames)
+  )
+})
+
 test_that("linear_reg - glmnet - regression", {
   skip_if_not_installed("glmnet")
   skip_if_not_installed("modeldata")
