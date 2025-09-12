3a7d320d27b919882e3d2d085b78b7f2a5fb8f44
Hannah Frick <hannah@posit.co>
2025-09-12 15:04:42 +0100

    go back to shorter format
    based on https://github.com/tidymodels/stacks/pull/242

--- tests/testthat/test-stacks-columns.R
+++ tests/testthat/test-stacks-columns.R
@@ -14,9 +14,7 @@ library(tidyr)
 library(dplyr)
 
 test_that("stacks can accommodate outcome levels that are not valid colnames", {
-  # skip on pre-0.2.4
-  skip_if(utils::packageVersion("stacks") < "0.2.3.9001")
-  skip_if_not_installed("tune", "1.3.0.9006")
+  skip_if_not_installed("stacks", "1.1.1.9001")
 
   data("penguins")
 
@@ -45,7 +43,7 @@ test_that("stacks can accommodate outcome levels that are not valid colnames", {
   )
 
   expect_true(inherits(data_st, "data_stack"))
-  expect_true(".pred_Adelie.1_tunedpre0_mod2_post0" %in% colnames(data_st))
+  expect_true(".pred_Adelie.1_tuned_0_2_0" %in% colnames(data_st))
 
   # glmnet will likely present warnings
   suppressMessages(
